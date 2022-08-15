4a302db27e9e669015195c6b67d075e821b08475
sibel altay <altaysibel909@gmail.com>
2022-08-15 20:10:57 +0300

    3.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,7 +1,8 @@
 public class Day1Git {
     public static void main(String[] args) {
 
-        System.out.println("Git 1. Deneme");
-
+        System.out.println("Hello Git 1. Deneme");
+        System.out.println("Hello Git 2. Deneme");
+        System.out.println("Hello Git 3. Deneme");
     }
 }
